d61d257ca5d3d2ab30e317eb313ec7040816f472
Steve Turley <steve@turleylabs.com>
2020-04-09 11:28:39 -0500

    WIP

---
+++ src/test/java/com/dojo/evercraft/EvercraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.evercraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvercraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
