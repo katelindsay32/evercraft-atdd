ed33dd148841c91ac1e4d5f69094bb7bb77d00ca
Steve Turley <steve@turleylabs.com>
2020-04-15 15:49:50 -0500

    "mob next [ci-skip]"

--- src/test/java/com/dojo/evercraft/cucumber/CharacterStepDefs.java
+++ src/test/java/com/dojo/evercraft/cucumber/CharacterStepDefs.java
@@ -1,6 +1,7 @@
 package com.dojo.evercraft.cucumber;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -27,21 +28,23 @@ public class CharacterStepDefs {
 //                        .withStatus(200)
 //                        .withHeader("Content-Type", "text/json")
 //                        .withBody("Darth Plagueis")));
+WebDriver driver = new ChromeDriver();
 
 
     @Given("a new game is started")
     public void aNewGameIsStarted() {
 
-        WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:8080/");
         driver.findElement(By.id("startButton")).click();
     }
 
     @When("I create a character")
     public void iCreateACharacter() {
+        driver.findElement(By.id("")).sendKeys("");
     }
 
     @Then("my character shows up in the game")
     public void myCharacterShowsUpInTheGame() {
+        throw new PendingException();
     }
 }
